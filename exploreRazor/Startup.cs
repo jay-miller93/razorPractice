1fa3b95c74b2c2909566b71159017b87a1ccd769
Jay <isaacsdaddy42293@gmail.com>
2017-07-24 11:58:23 -0400

    initial commit

---
+++ exploreRazor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(exploreRazor.Startup))]
+namespace exploreRazor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
